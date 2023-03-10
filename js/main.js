05c62a25ded1c133fc6a50fbc7d20d849af6f5d1
Shaun Burdick <github@shaunburdick.com>
2023-03-10 14:22:40 -0500

    Updating with some ChatGPT suggestions

--- js/main.js
+++ js/main.js
@@ -1,53 +1,38 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
-  var app = angular.module('sb', ['ui.router', 'ui.bootstrap']);
+  var app = angular.module("sb", ["ui.router", "ui.bootstrap"]);
 
-  app.config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise('/');
+  app.config([
+    "$stateProvider",
+    "$urlRouterProvider",
+    function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise("/");
 
       $stateProvider
-        .state('home', {
-          url:'/',
-          templateUrl: 'home'
+        .state("home", {
+          url: "/",
+          templateUrl: "home",
         })
-        .state('personal', {
-          url:'/personal',
-          templateUrl: 'personal'
+        .state("personal", {
+          url: "/personal",
+          templateUrl: "personal",
         })
-        .state('professional', {
-          url:'/professional',
-          templateUrl: 'professional'
+        .state("professional", {
+          url: "/professional",
+          templateUrl: "professional",
         });
-    }
+    },
   ]);
 
-  app.controller('MenuCtrl', ['$scope', '$state',
-    function($scope, $state) {
+  app.controller("MenuCtrl", [
+    "$scope",
+    "$state",
+    function ($scope, $state) {
       $scope.$state = $state;
-      $scope.emailHash = (Math.random()+1).toString(36)
-                                          .replace(/[^\w]/, '');
+      $scope.emailHash = (Math.random() + 1).toString(36).replace(/[^\w]/, "");
+      $scope.email = $scope.emailHash + "-site@shaunburdick.com";
       $scope.collapse = true;
-    }
+    },
   ]);
-
-  app.controller('SkillsCtrl', ['$scope',
-    function($scope) {
-      $scope.skills = [
-        {name: 'PHP', comfort: '95'},
-        {name: 'AngularJs', comfort: '75'},
-        {name: 'jQuery', comfort: '75'},
-        {name: 'Javascript', comfort: '75'},
-        {name: 'CSS', comfort: '65'},
-        {name: 'Bootstrap', comfort: '50'},
-        {name: 'MySQL', comfort: '80'},
-        {name: 'Perl', comfort: '40'},
-        {name: 'LAMP Administration', comfort: '50'},
-        {name: 'NodeJS', comfort: '30'},
-        {name: 'Neo4J', comfort: '10'},
-        {name: 'Scrum', comfort: '80'}
-      ];
-    }
-  ]);
-}());
+})();
