dd9b7dec0ddeb7116100d3801994af6f78475724
Shaun Burdick <github@shaunburdick.com>
2024-08-02 19:36:40 -0400

    Remove Domain

--- src/Tracker.tsx
+++ src/Tracker.tsx
@@ -2,7 +2,6 @@ import Plausible from 'plausible-tracker';
 import { createContext } from 'react';
 
 const tracker = Plausible({
-  domain: 'shaunburdick.com',
   apiHost: 'https://analytics.public.burdick.dev'
 });
 
