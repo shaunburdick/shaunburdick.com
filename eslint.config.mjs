0e72aa9cc1b15feed5bd69c6f3164b37168c8101
Shaun Burdick <github@shaunburdick.com>
2024-08-12 08:50:28 -0400

    Bumped lint rules to 2.0.1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,9 @@
 import shaunburdick from 'eslint-config-shaunburdick';
-import reactPlugin from 'eslint-plugin-react';
-import globals from 'globals';
 
 export default [
     ...shaunburdick.config.js,
     ...shaunburdick.config.ts,
-    {
-        files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-        ...reactPlugin.configs.flat.recommended,
-        ...reactPlugin.configs.flat['jsx-runtime'],
-        languageOptions: {
-            ...reactPlugin.configs.flat.recommended.languageOptions,
-            globals: {
-                ...globals.serviceworker,
-                ...globals.browser,
-            },
-        },
-    },
+    ...shaunburdick.config.react,
     {
         ignores: [
             'build/**/*'
