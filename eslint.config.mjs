2de2c1382f31dd8faa0dddee083666f418b4bd8f
Shaun Burdick <github@shaunburdick.com>
2024-12-26 19:11:13 -0500

    Update linting to latest version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import shaunburdick from 'eslint-config-shaunburdick';
+// import here to solve issues with ESM import when referencing file directly
+import webpackConfig from './webpack.development.js';
 
 export default [
     ...shaunburdick.config.js,
@@ -8,5 +10,14 @@ export default [
         ignores: [
             'build/**/*'
         ]
+    },
+    {
+        settings: {
+            'import/resolver': {
+                webpack: {
+                    config: webpackConfig
+                }
+            }
+        }
     }
 ];
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,6 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// Weird import resolver issue, just ignore it
+// eslint-disable-next-line import/no-unresolved
 import '@testing-library/jest-dom';
 
 global.console = {
